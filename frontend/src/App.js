190a911791168918011fa70c7365370539a98683
Ethan Hubbartt <ehubbartt@gmail.com>
2021-08-09 12:30:24 -0400

    nav bar is now fully functional with the data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import HomePage from "./Pages/HomePage";
-import JobPage from "./Pages/JobPage";
+import { navData } from "./data";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./Components/Navbar";
@@ -9,17 +8,31 @@ function App() {
   console.log(
     `sample URLs for testing:\nhttps://i.imgur.com/cAke54O.jpg\nhttps://i.imgur.com/b0apCDi.jpg?1\nhttps://i.imgur.com/C6q1LJ1.jpg?1\nhttps://i.imgur.com/wXMdv5q.jpg?1\nhttps://i.imgur.com/Mjf8um5.jpg`
   );
+
+  const allLinks = [];
+  for (let i = 0; i < navData.length; i++) {
+    let links = navData[i].links;
+    for (let j = 0; j < links.length; j++) {
+      allLinks.push(links[j]);
+    }
+  }
+
   return (
     <Router>
       <Navbar />
       <section className="main-section">
         <Switch>
-          <Route exact path="/">
-            <HomePage title="Home" />
-          </Route>
-          <Route path="/jobs">
-            <JobPage title="Job List" />
-          </Route>
+          {allLinks.map((link) => {
+            return link.exact ? (
+              <Route key={link.id} exact path={link.url}>
+                {link.page}
+              </Route>
+            ) : (
+              <Route key={link.id} path={link.url}>
+                {link.page}
+              </Route>
+            );
+          })}
         </Switch>
       </section>
     </Router>
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,5 +1,7 @@
 import { AiOutlineHome } from "react-icons/ai";
 import { CgWorkAlt } from "react-icons/cg";
+import JobPage from "./Pages/JobPage";
+import HomePage from "./Pages/HomePage";
 
 export const jobsData = [];
 export const job = {};
@@ -8,11 +10,29 @@ export const navData = [
   {
     sectionid: 1,
     section: "Main",
-    links: [{ id: 11, url: "/", text: "Home", icon: <AiOutlineHome /> }],
+    links: [
+      {
+        id: 11,
+        page: <HomePage title="Home" />,
+        url: "/",
+        text: "Home",
+        icon: <AiOutlineHome />,
+        exact: true,
+      },
+    ],
   },
   {
     sectionid: 2,
     section: "Orders",
-    links: [{ id: 21, url: "/jobs", text: "Jobs", icon: <CgWorkAlt /> }],
+    links: [
+      {
+        id: 21,
+        page: <JobPage title="Job List" />,
+        url: "/jobs",
+        text: "Jobs",
+        icon: <CgWorkAlt />,
+        exact: false,
+      },
+    ],
   },
 ];
