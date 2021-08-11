e26489f69336a6bb8a5f68a889a00bb3be9a5626
Ethan Hubbartt <ehubbartt@gmail.com>
2021-08-11 09:20:25 -0400

    refactored css files to be more organized

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./styles/index.css";
+import "./styles/navbar.css";
+import "./styles/modals.css";
+import "./styles/inputs-buttons.css";
+import "./styles/job-list.css";
 import App from "./App";
 import { AppProvider } from "./context";
 
