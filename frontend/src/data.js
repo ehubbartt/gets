f8853f5709dd220302340507e04626f011218f59
npatel-dot <npatel@gets-usa.com>
2021-08-11 13:51:56 -0400

    added few more sections in nav-bar

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -7,7 +7,8 @@ import InfoIcon from "@material-ui/icons/Info";
 import { BiBook } from "react-icons/bi";
 import { MdDescription } from "react-icons/md";
 import { FaChalkboardTeacher } from "react-icons/fa";
-import { GiCpu, GiReceiveMoney } from "react-icons/gi";
+import { GiCpu, GiReceiveMoney, GiTestTubes, GiMicroscope } from "react-icons/gi";
+import { FcTemplate } from "react-icons/fc";
 
 export const jobsData = [];
 export const job = {};
@@ -129,6 +130,59 @@ export const navData = [
         icon: <AiOutlineSafety style={iconStyles.navIcon} />,
         exact: false,
       },
+    ],
+  },
+  {
+    sectionid: 5,
+    section: "Tests",
+    links: [
+      {
+        id: 51,
+        page: <HomePage title="List Test Report" />,
+        url: "/test-reports",
+        text: "Test Reports",
+        icon: <GiTestTubes style={iconStyles.navIcon} />,
+        exact: false,
+      },
+      {
+        id: 52,
+        page: <HomePage title="List Test Report" />,
+        url: "/manage-template", 
+        text: "Manage Template", 
+        icon: <FcTemplate  style={iconStyles.navicon} />,
+        exact: false, 
+      },
+      {
+        id: 53,
+        page: <HomePage title="List Test Report" />,
+        url: "/Add-template", 
+        text: "Add Template", 
+        icon: <FcTemplate  style={iconStyles.navicon} />,
+        exact: false, 
+      },
+      {
+        id: 54,
+        page: <HomePage title="List Test Report" />,
+        url: "/cost-template", 
+        text: "Template Cost", 
+        icon: <FcTemplate  style={iconStyles.navicon} />,
+        exact: false, 
+      },
+    ],
+  },
+  {
+    sectionid: 6, 
+    section: "Equipment", 
+    links: [
+      {
+        id: 61,
+        page: <HomePage title="List of Equipments" />,
+        url: "/list-equipement", 
+        text: "List-Equipment", 
+        icon: <GiMicroscope  style={iconStyles.navicon} />,
+        exact: false,
+      },
+
     ],
   },
 ];
