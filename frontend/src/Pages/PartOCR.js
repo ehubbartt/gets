f772aa409385664eacf605042a12da6f4f3b8900
Ethan Hubbartt <ehubb@uw.edu>
2021-08-24 15:28:27 -0400

    you can now click to copy text on part OCR

--- frontend/src/Pages/PartOCR.js
+++ frontend/src/Pages/PartOCR.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "../Components/Header";
 import OrderImage from "../Components/OrderImage";
 import { postText } from "../services/get-text";
+import { FiClipboard } from "react-icons/fi";
 
 //TODO: comment all functions that are not already'
 //TODO: look for things that are not needed in functions and files
@@ -14,13 +15,25 @@ const PartOCR = ({ title }) => {
     setAllText(data);
   };
 
+  const copyText = async (e) => {
+    const text = e.target.innerHTML;
+    navigator.clipboard.writeText(text).then(
+      function () {
+        console.log("Async: Copying to clipboard was successful!");
+      },
+      function (err) {
+        console.error("Async: Could not copy text: ", err);
+      }
+    );
+  };
+
   return (
     <>
       <Header title={title}></Header>
       <div id="ocr">
         <div id="ocr-container">
           <OrderImage postData={postData} setImageBase64={setImageBase64} />
-          <AllText allText={allText} />
+          <AllText allText={allText} copyText={copyText} />
         </div>
       </div>
     </>
@@ -28,19 +41,25 @@ const PartOCR = ({ title }) => {
 };
 
 //TODO: style the text output
-const AllText = ({ allText }) => {
+const AllText = (props) => {
+  const { allText } = props;
   return (
     <div id="all-text-container">
       <h1>Text:</h1>
       {allText.map((text, idx) => {
-        return <Text key={idx} text={text} />;
+        return <Text key={idx} text={text} {...props} />;
       })}
     </div>
   );
 };
 
-const Text = ({ text }) => {
-  return <div id="text-container">{text}</div>;
+const Text = ({ text, copyText }) => {
+  return (
+    <div id="text-container" onClick={copyText}>
+      <p className="copy-text">{text}</p>
+      {<FiClipboard className="hidden clipboard-icon" />}
+    </div>
+  );
 };
 
 export default PartOCR;
